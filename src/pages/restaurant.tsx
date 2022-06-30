7be3bbb8906994903d6dfd844750f81731876b21
MoreeZ <moreez@protonmail.com>
2022-06-29 19:47:16 -0500

    added restaurant.tsx

--- src/pages/restaurant.tsx
+++ src/pages/restaurant.tsx
@@ -1,13 +1,121 @@
+import { graphql, useStaticQuery } from 'gatsby';
 import * as React from 'react';
 import Layout from '../components/Layout';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-export interface IGrindsInfoProps {
+type DetailsType = {
+    title: string,
+    description_main: string,
+    description_par1: string,
+    description_par2: string
 }
 
-export default function GrindsInfo(props: IGrindsInfoProps) {
+type DishType = {
+    name: string,
+    description: string,
+    price: string
+}
+
+type MenuType = {
+    subheading: string,
+    dishes: DishType[]
+}
+
+const Details = (props: { details: DetailsType }) => {
+    console.log(props.details);
+    return (
+        <div className="restaurant-screen scale-content-width">
+            <div className="restaurant-container">
+                <div className="heading" data-aos="fade-up">
+                    {props.details && props.details.title && props.details.title}
+                </div>
+                <div className="subheading" data-aos="fade-up" data-aos-delay="200">
+                    {props.details && props.details.description_main && props.details.description_main}
+                </div>
+                <div className="subheading2" data-aos="fade-up" data-aos-delay="300">
+                    {props.details && props.details.description_par1 && props.details.description_par1}
+                </div>
+                <div className="subheading2" data-aos="fade-up" data-aos-delay="400">
+                    {props.details && props.details.description_par2 && props.details.description_par2}
+                </div>
+            </div>
+            <div className="restaurant-darken"></div>
+        </div>
+    )
+}
+
+const Menu = (props: { menu: MenuType[] }) => {
+    return (
+        <div className='menu-container scale-content-width'>
+            <div className='menu-heading'>Menu</div>
+            <div className='menu-render-space block-content-width'>
+                {
+                    props.menu.map((submenu, index) => {
+                        return (
+                            <div className='each-submenu' key={index}>
+                                <div className='submenu-heading'>{submenu.subheading}</div>
+                                <table className='submenu-render-space'>
+                                    <tr>
+                                        <th>Dish</th>
+                                        <th>Description</th>
+                                        <th>Price</th>
+                                    </tr>
+                                    {submenu.dishes.map((dish, index2) => {
+                                        return (
+                                            <tr key={index2}>
+                                                <td className='dish-name'>{dish.name}</td>
+                                                <td className='dish-description'>{dish.description}</td>
+                                                <td className='dish-price'>{dish.price}</td>
+                                            </tr>
+                                        )
+                                    })}
+                                </table>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+const Restaurant = () => {
+    const { restaurantData, restaurantImages } = useStaticQuery(graphql`
+    query RestaurantQuery {
+        restaurantData: allDataJson (filter:{details:{title:{ne:null}}}){
+            edges {
+              node {
+                details {
+                  title
+                  description_main
+                  description_par1
+                  description_par2
+                }
+                menu {
+                  subheading
+                  dishes {
+                    name
+                    description
+                    price
+                  }
+                }
+              }
+            }
+          }
+    }
+  `)
+
+    React.useEffect(() => {
+        AOS.init({ duration: 600 });
+    }, [])
+
     return (
         <Layout hasNavbar hasFooter>
-            GrindsInfo content goes here
+            <Details details={restaurantData.edges[0].node.details} />
+            <Menu menu={restaurantData.edges[0].node.menu} />
         </Layout>
     );
 }
+
+export default Restaurant;
