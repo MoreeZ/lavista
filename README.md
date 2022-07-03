fa762371f3014f4bdc12096e581617394a7ca3f9
MoreeZ <moreez@protonmail.com>
2022-07-03 14:39:27 -0500

    Added reservation data and readme udpate

--- README.md
+++ README.md
@@ -16,7 +16,22 @@ Domain provider:
 1. Clone repository `git clone https://github.com/MoreeZ/lavista`
 2. Navigate into repository `cd lavista`
 3. Install all required local dependencies `npm install`
-4. If not yet installed, install global tools `npm i --location=global firebase-tools gatsby-cli`
+
+<details>
+<summary>### Firebase setup</summary>
+<br>
+1. If not yet installed, install firebase tools globally using `npm i --location=global firebase-tools gatsby-cli`
+2. Use `firebase login` and login in the browser to your google account.
+3. Use `firebase init hosting`
+  - "ready to process? (y/N)": Press Enter
+  - "What do you want to use as your public directory? (public)": Press Enter
+  - "Configure as a single-page app (rewrite all urls to /index.html)?": Press Enter
+  - "Set up automatic builds and deploys with GitHub?": n
+  - "File public/404.html already exists. Overwrite? (y/N)": Press Enter
+  - "File public/index.html already exists. Overwrite? (y/N)": Press Enter
+</details>
+
+
 
 ### Usage
 - Development: `npm run start`
--- src/pages/reservation.tsx
+++ src/pages/reservation.tsx
@@ -1,13 +1,102 @@
+import { graphql, Link, useStaticQuery } from 'gatsby';
 import * as React from 'react';
 import Layout from '../components/Layout';
 
-export interface IContactProps {
+type BasicDataType = {
+    title: string,
+    description: string,
+    button: ButtonType
 }
 
-export default function Contact(props: IContactProps) {
+type ButtonType = {
+    text: string,
+    link: string
+}
+
+const Contact = () => {
+    const { resData, resImages } = useStaticQuery(graphql`
+    query ReservationQuery {
+        resData: allDataJson(filter: {reservationSites: {title: {ne: null}}}) {
+          edges {
+            node {
+              reservationSites {
+                title
+                itemList {
+                  title
+                  description
+                  button {
+                    text
+                    link
+                  }
+                }
+              }
+              contactSection {
+                title
+                email
+                phone
+              }
+              locationSection {
+                title
+                iframe {
+                    src
+                    width
+                    height
+                    loading
+                }
+              }
+            }
+          }
+        }
+        resImages: allFile(filter: {relativeDirectory: {eq: "reservation"}}) {
+          edges {
+            node {
+              relativePath
+              childImageSharp {
+                gatsbyImageData
+              }
+            }
+          }
+        }
+      }      
+  `)
+    const { reservationSites, contactSection, locationSection } = resData.edges[0].node;
+
     return (
         <Layout hasNavbar hasFooter>
-            Contact content goes here
+            <div className="res-container">
+                <div className='res-websites-cont'>
+                    <div className='res-title'>{reservationSites.title}</div>
+                    <div className='res-render-space'>
+                        {reservationSites && reservationSites !== null && reservationSites.length > 0 && 
+                        reservationSites.itemList.map((item: BasicDataType, index: number) => {
+                            <div className='each-item' key={index}>
+                                <div className='item-title'>{item.title}</div>
+                                <div className='item-desc'>{item.description}</div>
+                                <Link to={item.button.link} className='item-button'>{item.button.text}</Link>
+                            </div>
+                        })}
+                    </div>
+                </div>
+                <div className="res-contact">
+                    <div className="heading">{contactSection.title}</div>
+                    <div className='phone-cont'>
+                        <div>Phone: </div>
+                        <div>{contactSection.phone}</div>
+                    </div>
+                    <div className='email-cont'>
+                        <div>Email: </div>
+                        <div>{contactSection.email}</div>
+                    </div>
+                </div>
+                <div className="lokalizacja-sub">
+                    <div className="heading">{locationSection.title}</div>
+                    <div className="map-wrapper">
+                        <iframe src={locationSection.iframe.src} width={locationSection.iframe.width} height={locationSection.iframe.height} loading={locationSection.iframe.loading}></iframe>
+                    </div>
+                </div>
+            </div>
         </Layout>
     );
 }
+
+export default Contact;
